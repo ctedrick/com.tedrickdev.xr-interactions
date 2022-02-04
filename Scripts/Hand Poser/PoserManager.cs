83eecf5e3bec0e8d1d710c2ca5bca9a6dc1c45fb
Cody Tedrick <codytedrick@gmail.com>
2022-02-03 21:15:28 -0800

    Add PoserManager

---
+++ Scripts/Hand Poser/PoserManager.cs
@@ -0,0 +1,58 @@
+﻿using TedrickDev.Utilities;
+using UnityEngine;
+
+namespace TedrickDev.XRPoser
+{
+    public class PoserManager : Singleton<PoserManager>
+    {
+        [Header("Pose Asset")]
+        public PoseData DefaultPose;
+
+        [Header("Scene Hands")]
+        public PoserHand LeftPoserHand;
+        public PoserHand RightPoserHand;
+        
+        [Header("Asset Hands")]
+        public PoserHand LeftPrefab;
+        public PoserHand RightPrefab;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            
+            if (!LeftPoserHand) {
+                Debug.LogWarning($"{nameof(LeftPoserHand)} is null, check reference on {gameObject}");
+                enabled = false;
+            }
+            
+            if (!RightPoserHand) {
+                Debug.LogWarning($"{nameof(RightPoserHand)} is null, check reference on {gameObject}");
+                enabled = false;
+            }
+        }
+        
+        public void ApplyPose(Handedness hand, PoseData pose)
+        {
+            if (hand == Handedness.Left) LeftPoserHand.SetPose(pose.LeftJoints);
+            else RightPoserHand.SetPose(pose.RightJoints);
+        }
+        
+        public void ApplyPose(PoserHand hand, PoseData pose)
+        {
+            if (hand.Type == Handedness.Left) LeftPoserHand.SetPose(pose.LeftJoints);
+            else RightPoserHand.SetPose(pose.RightJoints);
+        }
+        
+        public void ApplyDefaultPose(Handedness hand)
+        {
+            if (hand == Handedness.Left) LeftPoserHand.SetPose(DefaultPose.LeftJoints);
+            else RightPoserHand.SetPose(DefaultPose.RightJoints);
+        }
+        
+        public void ApplyDefaultPose(PoserHand hand)
+        {
+            if (hand.Type == Handedness.Left) LeftPoserHand.SetPose(DefaultPose.LeftJoints);
+            else RightPoserHand.SetPose(DefaultPose.RightJoints);
+        }
+    }
+}
