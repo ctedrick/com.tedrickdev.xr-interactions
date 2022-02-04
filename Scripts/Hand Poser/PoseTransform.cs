a1f738e3b74081c5117064452cc5850f31281efa
Cody Tedrick <codytedrick@gmail.com>
2022-02-03 21:14:56 -0800

    Add PoseTransform

---
+++ Scripts/Hand Poser/PoseTransform.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace TedrickDev.XRPoser
+{
+    [Serializable]
+    public struct PoseTransform
+    {
+        public Vector3 LocalPosition;
+        public Quaternion LocalRotation;
+    }
+}
