88d71f4b042ef9bf786bb22a37ddfb379ed8f581
Cody Tedrick <codytedrick@gmail.com>
2022-02-03 21:15:59 -0800

    Add Extensions

---
+++ Scripts/Utilities/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace TedrickDev.Utilities
+{
+    public static class Extensions
+    {
+        public static Vector3 RotatePointAroundPivot(this Vector3 point, Vector3 pivot, Vector3 angles)
+        {
+            var direction = point - pivot;
+            direction = Quaternion.Euler(angles) * direction;
+            return direction + pivot;
+        }
+        
+        public static string ConvertToProjectRelativePath(this string path) => FileUtil.GetProjectRelativePath(path);
+    }
+}
