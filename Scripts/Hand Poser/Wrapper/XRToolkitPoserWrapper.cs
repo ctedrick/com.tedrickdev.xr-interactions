11ecec8278429dd3a114a63b872b6cbb3c978c81
Cody Tedrick <codytedrick@gmail.com>
2022-02-03 21:15:50 -0800

    Add XRToolkitPoserWrapper

---
+++ Scripts/Hand Poser/Wrapper/XRToolkitPoserWrapper.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+namespace TedrickDev.XRPoser.Wrapper
+{
+    [RequireComponent(typeof(XRGrabInteractable))]
+    public class XRToolkitPoserWrapper : MonoBehaviour
+    {
+        private PoserTool tool;
+        private XRBaseInteractable interactable;
+
+        protected void Awake()
+        {
+            tool = GetComponent<PoserTool>();
+            interactable = GetComponent<XRBaseInteractable>();
+            interactable.selectEntered.AddListener(OnSelectEntered);
+            interactable.selectExited.AddListener(OnSelectExited);
+        }
+
+        private void OnSelectEntered(SelectEnterEventArgs args)
+        {
+            var hand = args.interactorObject.transform.GetComponentInChildren<PoserHand>();
+            var interactor = args.interactorObject.transform.GetComponent<XRBaseInteractor>();
+            tool.ApplyPose(hand, interactor.attachTransform);
+        }
+
+        private void OnSelectExited(SelectExitEventArgs args)
+        {
+            var hand = args.interactorObject.transform.GetComponentInChildren<PoserHand>();
+            tool.ApplyDefaultPose(hand);
+        }
+    }
+}
